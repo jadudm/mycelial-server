7084a5ece12fe821d4f412c42edeb3a2e7933be0
Michael Somkuti <msomkuti@bates.edu>
2018-05-09 20:04:40 +0000

    changed port

--- runme.bash
+++ runme.bash
@@ -1,7 +1,7 @@
 source /usr/local/bin/virtualenvwrapper.sh
 mkvirtualenv mycelial-mural
 workon mycelial-mural
-pip install -r requirements.txt
-pip install -e mural
+pip install --user -r requirements.txt
+pip install --user -e mural
 
-gunicorn -w 3 -b 0.0.0.0:4000 mycelial:app
+gunicorn -w 3 -b 0.0.0.0:8080 mycelial:app
